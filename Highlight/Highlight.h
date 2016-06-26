bacec4546e65ddf034c07d80aaadc73645c9752b
satoshi-takano <satoshi.takano@dena.com>
2016-06-26 10:52:45 +0900

    Initial commit

---
+++ Highlight/Highlight.h
@@ -0,0 +1,19 @@
+//
+//  Highlight.h
+//  Highlight
+//
+//  Created by Satoshi Takano on 2016/06/26.
+//  Copyright © 2016年 Satoshi Takano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Highlight.
+FOUNDATION_EXPORT double HighlightVersionNumber;
+
+//! Project version string for Highlight.
+FOUNDATION_EXPORT const unsigned char HighlightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Highlight/PublicHeader.h>
+
+
---
+++ Sources/Highlight.swift
@@ -0,0 +1,9 @@
+//
+//  Highlight.swift
+//  Highlight
+//
+//  Created by Satoshi Takano on 2016/06/26.
+//  Copyright © 2016年 Satoshi Takano. All rights reserved.
+//
+
+import Foundation
